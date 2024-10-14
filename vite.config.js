ca99ee23034465cc85ccfc349ac0cbd66e7fbb5e
Pavel Yatsura <bedman211@gmail.com>
2024-10-14 15:44:50 +0300

    deploy config

--- vite.config.js
+++ vite.config.js
@@ -4,11 +4,5 @@ import react from "@vitejs/plugin-react-swc";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  css: {
-    preprocessorOptions: {
-      scss: {
-        silenceDeprecations: ["legacy-js-api"],
-      },
-    },
-  },
+  base: "/test-7/"
 });
